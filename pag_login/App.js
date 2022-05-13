13e3c4547296b9f2e967fc5f6f9210918d929608
Gabriel Evaristo Silva <gabrielevaristo7@hotmail.com>
2022-05-13 11:31:11 -0300

    Criação das  págin inicial, tela de login e tela de criação de conta

---
+++ pag_login/App.js
@@ -0,0 +1,258 @@
+import { Text, View, Button, TouchableOpacity } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View 
+      style={{ 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center' 
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'blue',
+          width: '300px',
+          height: '40px',
+          borderRadius: '20px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: 6
+        }}
+        onPress={() => navigation.navigate('Login')}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'monospace',
+            fontSize: '15px'
+          }}
+        >
+          Entrar
+        </Text>
+      </TouchableOpacity>
+    
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'blue',
+          width: '300px',
+          height: '40px',
+          borderRadius: '20px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: 6
+        }}
+        onPress={() => navigation.navigate('Criar conta')}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'monospace',
+            fontSize: '15px'
+          }}
+        >
+          Criar conta
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function Login({ navigation }) {
+  return (
+    <View 
+      style={{ 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center' 
+      }}>
+      <label
+        style={{
+          fontWeight: 'bold',
+          fontSize: '20px',
+          fontFamily: 'monospace',
+          margin: 4
+        }}
+      >
+        E-mail
+      </label>
+      <input
+        placeholder='Ex.: meuemail@email.com'
+        style={{
+          width: '300px',
+          height: '40px',
+          borderRadius: '5px',
+          fontSize: '15px',
+          margin: 4
+        }}
+      />
+
+      <label
+        style={{
+          fontWeight: 'bold',
+          fontSize: '20px',
+          fontFamily: 'monospace',
+          margin: 4
+        }}
+      >
+        Senha
+      </label>
+      <input
+        placeholder='Digite sua senha'
+        style={{
+          width: '300px',
+          height: '40px',
+          borderRadius: '5px',
+          fontSize: '15px',
+          margin: 4
+        }}
+      />
+  
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'blue',
+          width: '300px',
+          height: '40px',
+          borderRadius: '20px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: 10
+        }}
+      onPress={() => navigation.navigate('Criar conta')}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'monospace',
+            fontSize: '15px'
+          }}
+        >
+          Entrar
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function CriarConta({ navigation }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+      <label
+        style={{
+          fontWeight: 'bold',
+          fontSize: '20px',
+          fontFamily: 'monospace',
+          margin: 4
+        }}
+      >
+        E-mail
+      </label>
+      <input
+        placeholder='Digite seu melhor e-mail'
+        style={{
+          width: '300px',
+          height: '40px',
+          borderRadius: '5px',
+          fontSize: '15px',
+          margin: 4
+        }}
+      />
+
+      <label
+        style={{
+          fontWeight: 'bold',
+          fontSize: '20px',
+          fontFamily: 'monospace',
+          margin: 4
+        }}
+      >
+        Crie uma senha
+      </label>
+      <input
+        placeholder='Digite aqui'
+        style={{
+          width: '300px',
+          height: '40px',
+          borderRadius: '5px',
+          fontSize: '15px',
+          margin: 4
+        }}
+      />
+
+      <label
+        style={{
+          fontWeight: 'bold',
+          fontSize: '20px',
+          fontFamily: 'monospace',
+          margin: 4
+        }}
+      >
+        Repita a senha
+      </label>
+      <input
+        placeholder='Digite aqui'
+        style={{
+          width: '300px',
+          height: '40px',
+          borderRadius: '5px',
+          fontSize: '15px',
+          margin: 4
+        }}
+      />
+
+      <Text 
+        style={{ margin: 15 }}
+      >
+        Eu li e aceito os 
+        <Text
+        style={{ color: 'blue' }}
+        > Termos de Serviço</Text>
+      </Text>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'blue',
+          width: '300px',
+          height: '40px',
+          borderRadius: '20px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          margin: 10
+        }}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'monospace',
+            fontSize: '15px'
+          }}
+        >
+          Criar conta
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Foo App" component={HomeScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Criar conta" component={CriarConta} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
